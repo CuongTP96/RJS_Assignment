b14729e0de46a8f6a0091378ac0d799a20d3c87f
Cuong <cuongtpfx15611@funix.edu.vn>
2022-08-23 21:48:05 +0700

    Lab_02-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello - Đây là bài LAB 02.1 của Cường</h1>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
