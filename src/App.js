1200bed8895df27ed2b2fe8ad69c8c8be3549aa4
Cuong <cuongtpfx15611@funix.edu.vn>
2022-09-02 21:58:22 +0700

    Lab 04.1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponent";
-import "./App.css";
-// Sau khi tải file staffs.jsx về bạn import dữ liệu vào App và truyền STAFFS vào component StaffList;
-import StaffList from "./components/StaffListComponent";
-import { STAFFS } from "./shared/staffs";
+import Main from "./components/MainComponent";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      staffs: STAFFS,
-    };
-  }
-
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        <StaffList staffs={this.state.staffs} />
+        <Main />
       </div>
     );
   }
