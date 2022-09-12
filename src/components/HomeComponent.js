a7929934d4559d6b224a29bce20824790f46905c
Cuong <cuongtpfx15611@funix.edu.vn>
2022-09-12 21:23:55 +0700

    Lab_05.3

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,46 @@
 import React from "react";
 
+// Lab 5.3
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
+
+// Sử dụng toán tử 3 ngôi: nếu có item.designation thì sẽ render và ngược lại thì không
+function RenderCard({ item }) {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+// function Home nhận props từ Main và render ra 3 cột dữ liệu thông qua hàm RenderCard
 function Home(props) {
   return (
     <div className="container">
-      <h4>Home</h4>
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dish} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
-import DishDetail from "./DishdetailComponent";
-import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-
-//Lab 05.2 Gọi 2 file Home với react router
 import Home from "./HomeComponent";
 import { Switch, Redirect, Route } from "react-router-dom";
+
+//Lab 05.3
+import Contact from "./ContactComponent";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
@@ -16,13 +18,23 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   // Hiển thị định hướng đường dẫn
   render() {
-    const HomePage = (props) => {
-      return <Home />;
+    const HomePage = () => {
+      // Truyền Main vào Home
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -36,6 +48,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
 
